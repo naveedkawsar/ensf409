6d30c78fc5756893ea22e05529270d3110b0f54d
naveedkawsar <naveedkawsar@github.com>
2017-01-26 17:51:05 -0700

    Working code. To be formatted and further tested.
    Signed-off-by: naveedkawsar <naveedkawsar@github.com>

--- Lab1/src/MultiArray.java
+++ Lab1/src/MultiArray.java
@@ -14,7 +14,15 @@ public class MultiArray {
 	
 	public void reverse_chars(int row, String sentence)
 	{
-
+		char[] letters = sentence.toCharArray();	// Convert sentence to an array of chars
+		
+		int j = 0;
+		for (int i = sentence.length() - 1; i >= 0; i--)
+		{
+	    	/* Turn individual chars back into strings, and store into array row backwards */
+			sentence_array[row][j] = Character.toString(letters[i]);
+			j++;
+		}
 	}
 	
 	public void reverse_words(int row, String sentence)
@@ -32,7 +40,16 @@ public class MultiArray {
 	
 	public void capitalize_fifths(int row, String sentence)
 	{
+		char[] letters = sentence.toCharArray();	// Convert sentence to an array of chars
 		
+		for (int i = 0; i < sentence.length() ; i++)
+		{
+			if ((i % 5) == 0)
+				sentence_array[row][i] = Character.toString(Character.toUpperCase(letters[i]));
+			else
+				sentence_array[row][i] = Character.toString(letters[i]);
+		}
+				
 	}
 
 	public void print_multi_array()
@@ -49,21 +66,21 @@ public class MultiArray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		MultiArray manipulate_sentences = new MultiArray();
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("\nPlease enter your first sentence.\n");
-	    String sentence = input.nextLine();
-	    manipulate_sentences.reverse_chars(0, sentence);
+	    String sentence_one = input.nextLine();
+	    manipulate_sentences.reverse_chars(0, sentence_one);
 	    
 		System.out.print("\nPlease enter your second sentence.\n");
-		//String sentence = input.nextLine();
-		//manipulate_sentences.reverse_words(1, sentence);
+		String sentence_two = input.nextLine();
+		manipulate_sentences.reverse_words(1, sentence_two);
 		
 		System.out.print("\nPlease enter your third sentence.\n");
-		//String sentence = input.nextLine();
-		//manipulate_sentences.capitalize_fifths(2, sentence);
+		String sentence_three = input.nextLine();
+		manipulate_sentences.capitalize_fifths(2, sentence_three);
 		
 		manipulate_sentences.print_multi_array();
 
